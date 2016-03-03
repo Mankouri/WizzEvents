9c37c467f9ebf53022d57acc8d284a29750a9bd0
chesnais-mymy <chesnais@et.esiea.fr>
2016-03-03 18:29:35 +0100

    page préference, ajout de spinners

--- app/src/main/java/esiea/wizzevents/PrefActivity.java
+++ app/src/main/java/esiea/wizzevents/PrefActivity.java
@@ -1,23 +1,105 @@
 package esiea.wizzevents;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 /**
  * Created by Jalil on 08/02/2016.
  */
 public class PrefActivity extends ActionBarActivity implements View.OnClickListener {
     Button buttonValidPref;
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    Object divertissements = null,soldes=null,culture=null,sport=null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pref);
 
         buttonValidPref = (Button) findViewById(R.id.buttonValidPref);
-
         buttonValidPref.setOnClickListener(this);
+
+        spinner = (Spinner) findViewById(R.id.spinner2);
+        adapter = ArrayAdapter.createFromResource(this, R.array.divertissements, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+                divertissements = parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.soldes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+                soldes = parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner = (Spinner) findViewById(R.id.spinner3);
+        adapter = ArrayAdapter.createFromResource(this, R.array.culture, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+                culture = parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner = (Spinner) findViewById(R.id.spinner4);
+        adapter = ArrayAdapter.createFromResource(this, R.array.sport, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+                sport = parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
     @Override
